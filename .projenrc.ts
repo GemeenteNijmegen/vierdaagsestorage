9fbfdc2862a802cae2f4db2bd02c0893b37e84ea
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2025-02-24 11:58:39 +0100

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,13 @@
+import { GemeenteNijmegenCdkApp } from '@gemeentenijmegen/projen-project-type';
+const project = new GemeenteNijmegenCdkApp({
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  devDeps: ['@gemeentenijmegen/projen-project-type'],
+  name: 'vierdaagsefeestenstorage',
+  projenrcTs: true,
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
